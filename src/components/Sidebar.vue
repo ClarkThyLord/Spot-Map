f354920e4f1a326a26adf1785e2693e349c127bd
ClarkThyLord <ccpotential@gmail.com>
2020-09-04 19:53:28 -0700

    cleaning up ui
    Clark was here :V

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,40 +1,63 @@
 <template>
   <f7-panel left>
-      <div style="display: flex; flex-direction: column; height: 100%;">
-        <div style="margin: 15px; text-align: center;">
-          <img src="~/static/Spot-Map.logo.png"  style="height: 40px; height: 40px; border-radius: 50%"/>
-        </div>
-
-        <f7-list style="flex: 1; margin-top: 0;">
-          <f7-list-item title="Map" link="#"></f7-list-item>
-          <f7-list-item title="History" link="#"></f7-list-item>
-          <f7-list-item title="Alert COVID" link="#"></f7-list-item>
-          <f7-list-item title="Generate QR" link="#"></f7-list-item>
-        </f7-list>
-        
-        <div style="margin: 15px; text-align: center;">
-          <f7-link style="margin-bottom: 5px;">Business? Sign-in here</f7-link>
-          <br/>
-          <f7-link href="https://github.com/ClarkThyLord/Spot-Map#about" external target="_blank">
-            <f7-icon f7="info_circle"></f7-icon>
-          </f7-link>
-          <f7-link href="https://github.com/ClarkThyLord/Spot-Map/issues" external target="_blank">
-            <f7-icon f7="question_circle"></f7-icon>
-          </f7-link>
-          <f7-link href="https://github.com/ClarkThyLord/Spot-Map" external target="_blank">
-            <f7-icon f7="logo_github"></f7-icon>
-          </f7-link>
-        </div>
+    <div class="sidebar-content">
+      <div class="logo-space">
+        <img src="~/static/Spot-Map.logo.png" class="logo" />
       </div>
-    </f7-panel>
+
+      <f7-list class="sidebar-items">
+        <f7-list-item title="Map" link="#"></f7-list-item>
+        <f7-list-item title="History" link="#"></f7-list-item>
+        <f7-list-item title="Alert COVID" link="#"></f7-list-item>
+        <f7-list-item title="Generate QR" link="#"></f7-list-item>
+      </f7-list>
+
+      <div class="sidebar-info">
+        <f7-link style="margin-bottom: 5px;">Business? Sign-in here</f7-link>
+        <br />
+        <f7-link href="https://github.com/ClarkThyLord/Spot-Map#about" external target="_blank">
+          <f7-icon f7="info_circle"></f7-icon>
+        </f7-link>
+        <f7-link href="https://github.com/ClarkThyLord/Spot-Map/issues" external target="_blank">
+          <f7-icon f7="question_circle"></f7-icon>
+        </f7-link>
+        <f7-link href="https://github.com/ClarkThyLord/Spot-Map" external target="_blank">
+          <f7-icon f7="logo_github"></f7-icon>
+        </f7-link>
+      </div>
+    </div>
+  </f7-panel>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+.sidebar-content {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
 
+.logo-space {
+  margin: 15px;
+  text-align: center;
+}
+
+.logo-space .logo {
+  height: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+
+.sidebar-items {
+  flex: 1;
+  margin-top: 0;
+}
+
+.sidebar-info {
+  margin: 15px;
+  text-align: center;
+}
 </style>

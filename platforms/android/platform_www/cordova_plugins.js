354de2772de41705b0a67138f782bacbf58e575b
ClarkThyLord <ccpotential@gmail.com>
2020-09-10 14:18:49 -0700

    reattempting to fix project
    Clark was here :V

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -30,8 +30,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
     "cordova-plugin-qrcodejs": "1.0.0",
-    "cordova-plugin-qrscanner": "3.0.1",
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-qrscanner": "3.0.1"
   };
 });
--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -38,9 +38,9 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-whitelist": "1.3.4",
     "cordova-plugin-qrcodejs": "1.0.0",
-    "cordova-plugin-qrscanner": "3.0.1",
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-qrscanner": "3.0.1"
 }
 // BOTTOM OF METADATA
 });

87e09d6fb53ae5837fe0f3e345f68f0639fa4b07
D-Chenkov <ChenkovvoknehC@gmail.com>
2020-09-14 09:50:43 -0700

    Added QR display

--- src/pages/generate-qr.vue
+++ src/pages/generate-qr.vue
@@ -1,32 +1,38 @@
 <template>
   <f7-page>
+    <img v-bind:src="QR" alt="Something went wrong" />
   </f7-page>
 </template>
 
 <script>
-export default {  
-  mounted: function()
-  {
-let options = {
-    width: 256,
-    height: 256,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
+export default {
+  data: function () {
+    return { QR: "" };
+  },
+  mounted: function () {
+    let options = {
+      width: 256,
+      height: 256,
+      colorDark: "#000000",
+      colorLight: "#ffffff",
+    };
+
+    window.cordova.plugins.qrcodejs.encode(
+      "TEXT_TYPE",
+      "david@marin.com",
+      (base64EncodedQRImage) => {
+        console.info("QRCodeJS response is " + base64EncodedQRImage);
+        //TODO: use your base64EncodedQRImage
+        this.QR = base64EncodedQRImage; 
+      },
+      (err) => {
+        console.error("QRCodeJS error is " + JSON.stringify(err));
+      },
+      options
+    );
+  },
 };
- 
-window.cordova.plugins.qrcodejs.encode('TEXT_TYPE', 'myemail@mydomain.me', (base64EncodedQRImage) => {
-    console.info('QRCodeJS response is ' + base64EncodedQRImage);
-  //TODO: use your base64EncodedQRImage
-}, (err) => {
-    console.error('QRCodeJS error is ' + JSON.stringify(err));
-}, options);
-  }
-  
-}
-
-
 </script>
 
 <style>
-
 </style>

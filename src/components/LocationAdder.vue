949e8fcbbb8dd49aec04ff4d715e8d44574dd7ee
ClarkThyLord <ccpotential@gmail.com>
2020-09-25 21:11:04 -0700

    progress on internalization
    Clark was here :V

--- src/components/LocationAdder.vue
+++ src/components/LocationAdder.vue
@@ -1,22 +1,24 @@
 <template>
   <f7-popup
-    class="demo-popup"
+    class="location-adder"
     :opened="popupOpened"
     @popup:closed="popupOpened = false"
   >
     <f7-page>
-      <f7-navbar title="Location Adder">
+      <f7-navbar :title="internalization[session.language]['locadd']">
         <f7-nav-right>
-          <f7-link popup-close>Close</f7-link>
+          <f7-link popup-close>{{
+            internalization[session.language]["close"]
+          }}</f7-link>
         </f7-nav-right>
       </f7-navbar>
 
       <f7-block>
         <f7-list no-hairlines-md>
           <f7-list-input
-            label="Name"
             type="text"
-            placeholder="Location Name"
+            :label="internalization[session.language]['locname']"
+            :placeholder="internalization[session.language]['locname']"
             :value="name"
             @input="name = $event.target.value"
             clear-button
@@ -27,9 +29,9 @@
             <f7-col>
               <f7-list no-hairlines-md>
                 <f7-list-input
-                  label="Latitude"
                   type="text"
-                  placeholder="Latitude"
+                  :label="internalization[session.language]['lat']"
+                  :placeholder="internalization[session.language]['lat']"
                   :value="latitude"
                   @input="latitude = $event.target.value"
                   clear-button
@@ -40,9 +42,9 @@
             <f7-col>
               <f7-list no-hairlines-md>
                 <f7-list-input
-                  label="Longitude"
                   type="text"
-                  placeholder="Longitude"
+                  :label="internalization[session.language]['lng']"
+                  :placeholder="internalization[session.language]['lng']"
                   :value="longitude"
                   @input="longitude = $event.target.value"
                   clear-button
@@ -53,7 +55,7 @@
           </f7-row>
 
           <div style="display: flex; justify-content: center">
-            <f7-button @click="add_location">ADD</f7-button>
+            <f7-button @click="add_location">{{ internalization[session.language]["add"] }}</f7-button>
           </div>
         </f7-list>
       </f7-block>
@@ -65,10 +67,29 @@
 export default {
   data() {
     return {
+      session: window.Session,
       name: "",
       latitude: "",
       longitude: "",
       popupOpened: false,
+      internalization: {
+        english: {
+          locadd: "Location Adder",
+          close: "Close",
+          locname: "Location Name",
+          lat: "Latitude",
+          lng: "Longitude",
+          add: "ADD",
+        },
+        spanish: {
+          locadd: "Agregar Ubicación",
+          close: "Cerrar",
+          locname: "Nombre de Ubicación",
+          lat: "Latitud",
+          lng: "Longitud",
+          add: "AGREGAR",
+        },
+      },
     };
   },
   methods: {
@@ -79,8 +100,8 @@ export default {
       this.popupOpened = true;
     },
     add_location: function () {
-        this.$emit("add", this.name, this.latitude, this.longitude);
-        this.popupOpened = false;
+      this.$emit("add", this.name, this.latitude, this.longitude);
+      this.popupOpened = false;
     },
   },
 };
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,35 +3,35 @@
     <f7-page login-screen>
       <f7-login-screen-title>
         <img src="~/static/Spot-Map.logo.png" class="logo" />
-        <h3 style="margin-top: 0px;">BUSINESS</h3>
+        <h3 style="margin-top: 0px;">{{ internalization[session.language]["business"] }}</h3>
       </f7-login-screen-title>
 
       <f7-list form>
         <f7-list-input
-          label="Username"
-          placeholder="Username"
-          :value="email"
-          @input="email = $event.target.value"
+          :label="internalization[session.language]['username']"
+          :placeholder="internalization[session.language]['username']"
+          :value="username"
+          @input="username = $event.target.value"
         ></f7-list-input>
         <f7-list-input
-          label="Password"
+          :label="internalization[session.language]['password']"
           type="password"
-          placeholder="Password"
+          :placeholder="internalization[session.language]['password']"
           :value="password"
           @input="password = $event.target.value"
         ></f7-list-input>
 
         <f7-list v-if="registering" style="margin-top: 0px;">
           <f7-list-input
-            label="Re-Enter Password"
             type="password"
-            placeholder="Password"
+            :label="internalization[session.language]['repassword']"
+            :placeholder="internalization[session.language]['repassword']"
             :value="confirm_password"
             @input="confirm_password = $event.target.value"
           ></f7-list-input>
           <f7-list-input
-            label="Business Address"
-            placeholder="Business Address"
+            :label="internalization[session.language]['adress']"
+            :placeholder="internalization[session.language]['adress']"
             :value="address"
             @input="address = $event.target.value"
           ></f7-list-input>
@@ -40,14 +40,14 @@
 
       <f7-list style="overflow: hidden !important;">
         <div style="display: flex; justify-content: center;">
-          <f7-button v-if="registering" @click="register">Register</f7-button>
-          <f7-button v-else @click="sign_in">Login</f7-button>
-          <f7-button @click="cancel">Cancel</f7-button>
+          <f7-button v-if="registering" @click="register">{{ internalization[session.language]["register"] }}</f7-button>
+          <f7-button v-else @click="sign_in">{{ internalization[session.language]["login"] }}</f7-button>
+          <f7-button @click="cancel">{{ internalization[session.language]["cancel"] }}</f7-button>
         </div>
 
         <f7-block-footer>
-          <f7-link v-if="registering" href @click="registering=false">Have an account?</f7-link>
-          <f7-link v-else href @click="registering=true">Don't have an account?</f7-link>
+          <f7-link v-if="registering" href @click="registering=false">{{ internalization[session.language]["tacc"] }}</f7-link>
+          <f7-link v-else href @click="registering=true">{{ internalization[session.language]["dacc"] }}</f7-link>
         </f7-block-footer>
       </f7-list>
     </f7-page>
@@ -60,10 +60,37 @@ export default {
     return {
       open: false,
       registering: false,
-      email: "",
+      username: "",
       password: "",
       confirm_password: "",
       address: "",
+      session: window.Session,
+      internalization: {
+        english: {
+          "business": "BUSINESS",
+          "username": "Username",
+          "password": "Password",
+          "repassword": "Re-Enter Password",
+          "adress": "Business Address",
+          "register": "Register",
+          "login": "Login",
+          "cancel": "Cancel",
+          "tacc": "Have an account?",
+          "dacc": "Don't have an account?"
+        },
+        spanish: {
+          "business": "NEGOCIO",
+          "username": "Nombre de Usuario",
+          "password": "Contraseña",
+          "repassword": "Repite la Contraseña",
+          "adress": "Dirección de Negocios",
+          "register": "Registrarse",
+          "login": "Iniciar sesión",
+          "cancel": "Cancelar",
+          "tacc": "¿Tienes una cuenta?",
+          "dacc": "¿No tienes una cuenta?"
+        },
+      }
     };
   },
   methods: {
@@ -73,13 +100,13 @@ export default {
     },
     clear() {
       this.registering = false;
-      this.email = "";
+      this.username = "";
       this.password = "";
       this.confirm_password = "";
       this.address = "";
     },
     sign_in() {
-      window.Session.business_login(this.email, this.password);
+      window.Session.business_login(this.username, this.password);
       this.close();
     },
     register() {
